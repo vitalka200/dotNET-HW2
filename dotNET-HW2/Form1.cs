6bdc3d258ebf06e7df80d8d7b2589cc679f4ef44
Karabitski, Vitali <vitalka200@gmail.com>
2016-06-04 20:40:44 -0700

    Connection string fix

--- dotNET-HW2/Form1.cs
+++ dotNET-HW2/Form1.cs
@@ -14,7 +14,7 @@ namespace dotNET_HW2
 {
     public partial class Form1 : Form
     {
-        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\vitalka200\Dropbox\VisualStudio\2015\dotNET-HW2\dotNET-HW2\bin\Debug\HW2.mdf;Integrated Security=True";
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\HW2.mdf;Integrated Security=True;Connect Timeout=30";
 
         public Form1() {InitializeComponent();}
 
